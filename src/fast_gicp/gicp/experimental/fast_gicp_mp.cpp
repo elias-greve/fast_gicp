88c8f0c7843d99218aadc2f0019150d8781d19ca
egreve <elias@grevemail.de>
2024-02-17 17:28:20 +0100

    Change template definitions from XYZI to XYZNormal
    This is useful to transmit panoptic information.

--- src/fast_gicp/gicp/experimental/fast_gicp_mp.cpp
+++ src/fast_gicp/gicp/experimental/fast_gicp_mp.cpp
@@ -2,4 +2,5 @@
 #include <fast_gicp/gicp/experimental/fast_gicp_mp_impl.hpp>
 
 // template class fast_gicp::FastGICPMultiPoints<pcl::PointXYZ, pcl::PointXYZ>;
-template class fast_gicp::FastGICPMultiPoints<pcl::PointXYZI, pcl::PointXYZI>;
+template
+class fast_gicp::FastGICPMultiPoints<pcl::PointXYZINormal, pcl::PointXYZINormal>;
--- src/fast_gicp/gicp/fast_vgicp.cpp
+++ src/fast_gicp/gicp/fast_vgicp.cpp
@@ -1,6 +1,11 @@
 #include <fast_gicp/gicp/fast_vgicp.hpp>
 #include <fast_gicp/gicp/impl/fast_vgicp_impl.hpp>
 
-template class fast_gicp::FastVGICP<pcl::PointXYZ, pcl::PointXYZ>;
-template class fast_gicp::FastVGICP<pcl::PointXYZI, pcl::PointXYZI>;
-template class fast_gicp::FastVGICP<pcl::PointNormal, pcl::PointNormal>;
+template
+class fast_gicp::FastVGICP<pcl::PointXYZ, pcl::PointXYZ>;
+
+template
+class fast_gicp::FastVGICP<pcl::PointXYZINormal, pcl::PointXYZINormal>;
+
+template
+class fast_gicp::FastVGICP<pcl::PointNormal, pcl::PointNormal>;
